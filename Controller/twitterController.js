078af85154a9c0f0cd02df192dd411a0e6d7ea5d
Pinto-PINTO <marie334fe@gmail.com>
2024-03-21 22:25:52 -0400

    changed api keys and data

--- Controller/twitterController.js
+++ Controller/twitterController.js
@@ -4,11 +4,11 @@ const { TwitterApi } = require("twitter-api-v2");
 class TwitterModel {
   constructor() {
     this.client = new TwitterApi({
-      appKey: "O6FcMOpIOJcS7u9fA07NwahiB",
-      appSecret: "gMOYx9NAWkaFI0DbkC5GNNfg6BxxqTg27uX91TFvLawgdpRHdM",
-      accessToken: "1304198813356351491-3u0ZMcUzV4a3zUGbB64t3mZajvwtcv",
-      accessSecret: "rgU4HM7mw87IuUHQyHcYiuroMm1GaCnUXJ2zsygMh75c5",
-      bearerToken: "AAAAAAAAAAAAAAAAAAAAAKESswEAAAAAMVAGdOv2lsg078a6CIHFPrf%2Bjwg%3Dzqm9xPwGbgYwPZ1L5yagXZTjHERemY0I4ocGavXWYvZz7tLBBU",
+      appKey: "SUzCnbjwWtk2dnkzyWugsszwu",
+      appSecret: "IY2g4rt0fVH82CRzZWmwm2IQMrSUquCKrcIZCgrk7yvFgVrGUX",
+      accessToken: "1770974586009419776-ueY1yFh7FlikVkUlSM7RX4AgKi9Fm8",
+      accessSecret: "9hfYcENfd95VA54g1NXTIfsNrXeVqglaGhF8hgg9ZP02E",
+      bearerToken: "AAAAAAAAAAAAAAAAAAAAAD6%2BswEAAAAAmltPokoMfCA1PtuS3YBfy0wLzwQ%3DUIdmrQUcL06yLTNYT1IYAo32YqbL5St8oXAUTqzVLZbReNct7s",
     });
 
     this.rwClient = this.client.readWrite;
